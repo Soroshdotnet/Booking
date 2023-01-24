c6f0e2c0eaed050bfa70a4867bd036b4c5d7c394
soroshdotnet <sorosh.farahmand@gmail.com>
2023-01-24 16:25:28 +0100

    Add project files.

---
+++ Booking.Core/Entities/ApplicationUserGymClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Core.Entities
+{
+#nullable disable
+    public class ApplicationUserGymClass
+    {
+        public string ApplicationUserId { get; set; }
+        public int GymClassId { get; set; }
+
+        //Navigation prop.
+        public ApplicationUser ApplicationUser { get; set; }
+        public GymClass GymClass { get; set; }
+    }
+}
---
+++ Booking.Core/Entities/GymClass.cs
@@ -0,0 +1,19 @@
+﻿namespace Booking.Core.Entities
+{
+//#nullable disable
+    public class GymClass
+    {
+        int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        DateTime StartTime { get; set; }
+        TimeSpan Duration { get; set; }
+        //DateTime EndTime { get { return StartTime + Duration; } }
+        DateTime EndTime { get => StartTime + Duration; }
+        String Description { get; set; } = string.Empty;
+
+        public ICollection<ApplicationUserGymClass> AttendindMembers { get; set; } = new List<ApplicationUserGymClass>();
+
+
+    }
+}
