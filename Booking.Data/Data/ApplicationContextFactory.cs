ab45ec896af570c734dec9cbe2f5a60eb21cfa79
soroshdotnet <sorosh.farahmand@gmail.com>
2023-01-26 22:26:06 +0100

    ContextFactory / kommenterad

--- Booking.Data/Data/ApplicationContextFactory.cs
+++ Booking.Data/Data/ApplicationContextFactory.cs
@@ -1,18 +1,18 @@
-﻿using Booking.Web.Data;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
+﻿//using Booking.Web.Data;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Design;
 
-namespace Booking.Data
-{
-    public class ApplicationContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
-    {
-        public ApplicationDbContext CreateDbContext(string[] args)
-        {
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>();
-            options.UseSqlServer("Not used here");
+//namespace Booking.Data
+//{
+//    public class ApplicationContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+//    {
+//        public ApplicationDbContext CreateDbContext(string[] args)
+//        {
+//            var options = new DbContextOptionsBuilder<ApplicationDbContext>();
+//            options.UseSqlServer("Not used here");
 
-            return new ApplicationDbContext(options.Options);
+//            return new ApplicationDbContext(options.Options);
 
-        }
-    }
-}
+//        }
+//    }
+//}
