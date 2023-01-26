1114074bff9f9388e1db42a6468a22e869155708
soroshdotnet <sorosh.farahmand@gmail.com>
2023-01-26 22:24:48 +0100

    Removed home controller and views

--- Booking.Web/Program.cs
+++ Booking.Web/Program.cs
@@ -40,7 +40,7 @@ namespace Booking.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/GymClasses/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -54,7 +54,7 @@ namespace Booking.Web
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=GymClasses}/{action=Index}/{id?}");
             app.MapRazorPages();
 
             app.Run();
